a77f884973d7809d553d582b083845b5db6c9661
leveses <boda.zsombor10@gmail.com>
2020-09-14 10:59:38 +0200

    Beolvasas

--- A01BodaZsombor/Program.cs
+++ A01BodaZsombor/Program.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace A01BodaZsombor
 {
     class Program
     {
+        static int[] tomb = new int[1500];
         static void Main(string[] args)
         {
+            Beolvasas();
+            
+        }
 
+        private static void Beolvasas()
+        {
+            StreamReader olvas = new StreamReader("adatok.dat");
+            int x = 0;
+            while (!olvas.EndOfStream)
+            {
+                tomb[x] = int.Parse(olvas.ReadLine());
+                for (int i = 0; i < tomb.Length; i++)
+                {
+                   tomb[x] = tomb[i] * 2;
+                }
+            }
         }
     }
 }
